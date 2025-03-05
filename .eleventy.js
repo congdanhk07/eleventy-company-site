5df2b6de96d81d59f1bb0458ed614d262e2a7a1f
congdanhk07 <congdanhk07@gmail.com>
2025-03-05 09:55:51 +0700

    feat: setup sitemap and config function date

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,12 @@
 module.exports = function (eleventyConfig) {
+  const Image = require('@11ty/eleventy-img')
+  const { DateTime } = require('luxon')
   eleventyConfig.addPassthroughCopy('src/assets/css/style.css')
+  eleventyConfig.addPassthroughCopy({ 'src/robot.txt': 'robots.txt' })
   eleventyConfig.addPassthroughCopy('src/assets/images')
+  eleventyConfig.addFilter('postDate', (dateObj) => {
+    return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED)
+  })
   eleventyConfig.addCollection('page', function (collections) {
     return collections.getFilteredByTag('page').sort(function (a, b) {
       return a.data.order - b.data.order
