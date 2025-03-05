1d15bfadd178d1e1b998d87430cf08d5daf18221
congdanhk07 <congdanhk07@gmail.com>
2025-03-05 10:31:02 +0700

    feat: add tailwind config

--- .eleventy.js
+++ .eleventy.js
@@ -1,25 +1,22 @@
+const { DateTime } = require('luxon')
+const pageHeading = require('./src/_includes/shortcodes/pageHeading')
+const Image = require('@11ty/eleventy-img')
+
 module.exports = function (eleventyConfig) {
-  const Image = require('@11ty/eleventy-img')
-  const { DateTime } = require('luxon')
   eleventyConfig.addPassthroughCopy('src/assets/css/style.css')
   eleventyConfig.addPassthroughCopy({ 'src/robot.txt': 'robots.txt' })
   eleventyConfig.addPassthroughCopy('src/assets/images')
   eleventyConfig.addFilter('postDate', (dateObj) => {
     return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED)
   })
+
+  eleventyConfig.addShortcode('pageHeading', pageHeading)
+
   eleventyConfig.addCollection('page', function (collections) {
     return collections.getFilteredByTag('page').sort(function (a, b) {
       return a.data.order - b.data.order
     })
   })
-
-  // ADD THIS
-  eleventyConfig.addShortcode(
-    'headers',
-    (title, subtitle) =>
-      `<h1>${title}</h1>
-        <p>${subtitle}</p>`
-  )
 
   return {
     dir: {
---
+++ src/_includes/shortcodes/pageHeading.js
@@ -0,0 +1,8 @@
+const pageHeading = (title, subtitle) => {
+  return `
+    <h1 class='text-4xl font-semibold mt-2 '>${title}</h1>
+        <p class='text-lg '>${subtitle}</p>
+    `
+}
+
+module.exports = pageHeading
